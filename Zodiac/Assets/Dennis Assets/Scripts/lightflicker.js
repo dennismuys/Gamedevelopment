2c49e90bf7becf8f88075c8f01176697ea690fc7
dennismuys <dennismuys@hotmail.com>
2014-12-01 19:35:08 +0100

    Root Dump
    All files

---
+++ Zodiac/Assets/Dennis Assets/Scripts/lightflicker.js
@@ -0,0 +1,17 @@
+﻿public var minIntensity:float;
+public var maxIntensity:float;
+
+function Update () 
+    {
+        if ( Random.value > 0.9 ) //a random chance
+        {
+           if ( light.intensity >= minIntensity + 0.1 ) //if the light is on...
+           {
+             light.intensity = minIntensity; //turn it off
+           }
+           else
+           {
+             light.intensity = maxIntensity; //turn it on
+           }
+        }
+    }
